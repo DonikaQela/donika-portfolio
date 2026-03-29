ab3a5a4ba680573fb6daae18ce82aa0d3676bfe4
Donika Qela <donikaqela1@gmail.com>
2026-03-29 23:07:54 +0200

    Initial portfolio

--- src/components/portofolio/DonikaPortfolio.tsx
+++ src/components/portofolio/DonikaPortfolio.tsx
@@ -21,8 +21,8 @@ export default function DonikaPortfolio() {
     email: "donikaqela1@gmail.com",
     phone: "+38349445179",
     location: "Mitrovicë, Kosovo",
-    linkedin: "https://www.linkedin.com/",
-    github: "https://github.com/repos?q=owner%3A%40me",
+    linkedin: "https://www.linkedin.com/in/donika-qela/",
+    github: "https://github.com/DonikaQela",
     cvLink: "#",
   };
 
